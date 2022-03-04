540a310144d5d12a60bb4785dce9aa9e30e286fb
CR <931009686@qq.com>
2022-03-04 23:53:22 +0800

    feat: 025-入门-JavaAPI-文档-高级查询-分页查询 & 条件查询 & 字段查询

--- src/test/java/com/cr/es/test/EsDocQueryTest.java
+++ src/test/java/com/cr/es/test/EsDocQueryTest.java
@@ -26,10 +26,56 @@ public class EsDocQueryTest {
         );
 
         // 1.查询索引中全部的数据
+//        SearchRequest request = new SearchRequest();
+//        request.indices("user");
+//
+//        request.source(new SearchSourceBuilder().builder(QueryBuilders.matchAllQuery()));
+//
+//        SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
+
+        // 2.条件查询
+//        SearchRequest request = new SearchRequest();
+//        request.indices("user");
+//
+//        request.source(new SearchSourceBuilder().builder(QueryBuilders.termQuery("age", 30)));
+//
+//        SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
+
+        // 3.分页查询
+//        SearchRequest request = new SearchRequest();
+//        request.indices("user");
+//
+//        SearchSourceBuilder builder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+//        // （当前页码 - 1）* 每页显示数据条数
+//        builder.from(1);
+//        builder.size(2);
+//        request.source(builder);
+//
+//        SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
+
+        // 4.查询排序
+//        SearchRequest request = new SearchRequest();
+//        request.indices("user");
+//
+//        SearchSourceBuilder builder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+//
+//        builder.sort("age", SortOrder.DESC);
+//
+//        request.source(builder);
+//
+//        SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
+
+        // 5.过滤字段
         SearchRequest request = new SearchRequest();
         request.indices("user");
 
-        request.source(new SearchSourceBuilder().query(QueryBuilders.matchAllQuery()));
+        SearchSourceBuilder builder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+
+        String[] excludes = {"age"};
+        String[] includes = {};
+        builder.fetchSource(includes, excludes);
+
+        request.source(builder);
 
         SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
 
