ec609628ba512b05c42397e3e225f94397d6dac4
CR <931009686@qq.com>
2022-03-04 23:28:30 +0800

    feat: 023-入门-JavaAPI-文档-批量新增 & 批量删除

---
+++ src/test/java/com/cr/es/test/EsDocDeleteBatchTest.java
@@ -0,0 +1,39 @@
+package com.cr.es.test;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * @author cr
+ * @date 2022/2/27 18:42
+ */
+public class EsDocDeleteBatchTest {
+    @Test
+    public void test() throws IOException {
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        // 批量插入数据
+        BulkRequest request = new BulkRequest();
+
+        request.add(new DeleteRequest().index("user").id("1001"));
+        request.add(new DeleteRequest().index("user").id("1002"));
+        request.add(new DeleteRequest().index("user").id("1003"));
+
+        BulkResponse response = esClient.bulk(request, RequestOptions.DEFAULT);
+        System.out.println(response.getTook());
+        System.out.println(Arrays.toString(response.getItems()));
+
+        esClient.close();
+    }
+}
---
+++ src/test/java/com/cr/es/test/EsDocInsertBatchTest.java
@@ -0,0 +1,40 @@
+package com.cr.es.test;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * @author cr
+ * @date 2022/2/27 18:42
+ */
+public class EsDocInsertBatchTest {
+    @Test
+    public void test() throws IOException {
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        // 批量插入数据
+        BulkRequest request = new BulkRequest();
+
+        request.add(new IndexRequest().index("user").id("1001").source(XContentType.JSON, "name", "zhangsan"));
+        request.add(new IndexRequest().index("user").id("1002").source(XContentType.JSON, "name", "lisi"));
+        request.add(new IndexRequest().index("user").id("1003").source(XContentType.JSON, "name", "wangwu"));
+
+        BulkResponse response = esClient.bulk(request, RequestOptions.DEFAULT);
+        System.out.println(response.getTook());
+        System.out.println(Arrays.toString(response.getItems()));
+
+        esClient.close();
+    }
+}
